d9f3ad9bd3c52aadac635fa4c9878733c26a0dc2
Hang-coder <71011592+Hang-coder@users.noreply.github.com>
2020-10-07 19:25:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -34,9 +34,6 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-(function () {
-  var x = "Hello!!";  // I will invoke myself
-})();
 
 (function () {
 
